fda893916652e4a9f02a8b67014f9a632c5cb1b6
Jacques du Toit <jacquesd@fringecoding.com>
2026-06-09 11:31:50 +0200

    Setup tests to avoid warnings

--- web/vitest.setup.ts
+++ web/vitest.setup.ts
@@ -1,1 +1,35 @@
 import "@testing-library/jest-dom";
+import React from "react";
+import { vi } from "vitest";
+
+const routerFutureFlags = {
+  v7_startTransition: true,
+  v7_relativeSplatPath: true,
+} as const;
+
+vi.mock("react-router-dom", async () => {
+  const actual =
+    await vi.importActual<typeof import("react-router-dom")>(
+      "react-router-dom",
+    );
+
+  return {
+    ...actual,
+    BrowserRouter: ({
+      future,
+      ...props
+    }: React.ComponentProps<typeof actual.BrowserRouter>) =>
+      React.createElement(actual.BrowserRouter, {
+        ...props,
+        future: { ...routerFutureFlags, ...future },
+      }),
+    MemoryRouter: ({
+      future,
+      ...props
+    }: React.ComponentProps<typeof actual.MemoryRouter>) =>
+      React.createElement(actual.MemoryRouter, {
+        ...props,
+        future: { ...routerFutureFlags, ...future },
+      }),
+  };
+});
